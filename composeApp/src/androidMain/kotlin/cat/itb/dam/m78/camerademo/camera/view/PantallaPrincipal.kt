3cd094f6f1e1741f340014fd25e4ff9bb79e2547
vnritb <victor.naranjo@itb.cat>
2025-04-24 07:03:51 +0200

    Modularització separant el component per a demanar permisos de càmera

--- composeApp/src/androidMain/kotlin/cat/itb/dam/m78/camerademo/camera/view/PantallaPrincipal.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/dam/m78/camerademo/camera/view/PantallaPrincipal.kt
@@ -1,6 +1,7 @@
 package cat.itb.dam.m78.camerademo.camera.view
 
 import android.Manifest
+import android.content.Context
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
@@ -14,33 +15,23 @@ import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.LocalLifecycleOwner // Importació correcta per a compose
 
+
+
 //Pantalla principal que es fa servir des de la MainActivity
 @Composable
 fun PantallaPrincipal(modifier: Modifier = Modifier) {
     val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
     var hasCameraPermission by remember { mutableStateOf(false) }
 
-    //Objecte per gestionar el permís.  Llença la finestra de permisos
-    val permissionLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.RequestPermission(),
-        onResult = { granted ->
+    permisosDeCamera(
+        context = context,
+        granted = { granted ->
             hasCameraPermission = granted
         }
     )
 
-    // Demanar permís quan el composable entra a la composició si encara no s'ha concedit
-    LaunchedEffect(key1 = true) { // S'executa només una vegada a l'inici
-        // Comprovar si el permís ja ha estat concedit
-        val permissionStatus = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
-        //Pot ser que ja estigui concedit (si es va seleccionar l'opció de permetre sempre)
-        if (permissionStatus == android.content.pm.PackageManager.PERMISSION_GRANTED) {
-            hasCameraPermission = true
-        //En cas de no tenir permís (la primera vegada), es llença la finestra de permisos
-        } else {
-            permissionLauncher.launch(Manifest.permission.CAMERA)
-        }
-    }
+    val lifecycleOwner = LocalLifecycleOwner.current
+
 
     //Disseny de la UI
     Column(
